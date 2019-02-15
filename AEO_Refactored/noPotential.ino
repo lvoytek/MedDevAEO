41cedec588e3c97c7a18a02b2f28e777bb44356d
Lucas Boje <boje@email.arizona.edu>
2019-02-14 18:50:35 -0700

    Added w/o potentiometer. Feel free to change name

---
+++ AEO_Refactored/noPotential.ino
@@ -0,0 +1,49 @@
+const int dirPin = 7; 
+const int stepPin = 6; 
+const int sleepPin = 18;
+const int button1Pin = 2;
+const int button2Pin = 3;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(button1Pin, INPUT);
+  pinMode(button2Pin, INPUT);
+  pinMode(stepPin, OUTPUT);   
+  pinMode(dirPin, OUTPUT);
+  start();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //we should start this loop needing to step backward, so start going backward until you hit button1
+  while(digitalRead(button1Pin) == LOW) {
+    step_backward();
+  }
+  //now we need to step forward, go forward until you hit button2
+  while(digitalRead(button2Pin) == LOW) {
+    step_forward();
+  }
+}
+
+void start() {
+  // Steps forward until button2 is pressed, which would indicate you need to step backward. 
+  while(digitalRead(button2Pin) == LOW) {
+    step_forward();
+  }
+}
+
+void step_forward() {
+  Serial.println("stepping forward");
+  digitalWrite(dirPin, LOW);              
+  digitalWrite(stepPin, HIGH);
+  delayMicroseconds(500);
+}
+
+void step_backward() {
+  Serial.println("stepping backward");
+  digitalWrite(dirPin, HIGH);
+  digitalWrite(stepPin, HIGH);
+  delayMicroseconds(500);
+  digitalWrite(stepPin, LOW);
+}
